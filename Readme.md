268e6478b88197a6350cf77183059b1a0319ac19
HRY3737 <hyperry3737@gmail.com>
2021-12-04 00:38:08 +0900

    비동기로 변경 & NeedStrip 기준으로 공백 닉네임 제거

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
+### python-a2s 문서 링크입니다.
+#### https://github.com/Yepoleb/python-a2s
+
 ### 1. 2개의 라이브러리를 설치합니다.
 
 ```
@@ -6,3 +9,5 @@ pip install python-a2s
 ```
 
 ### 2. zip 파일 또는 git clone을 이용하여 이 프로젝트를 사용합니다.
+
+#### 기본 틀만 만들어놨으니 추가하고 싶은 기능 있으면 위에 있는 문서 보고 추가하세요.
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import a2s
 import discord
+import asyncio
+from discord import player
 from discord.ext import commands
 from discord.ext.commands.errors import MissingRequiredArgument
+from time import strftime,gmtime
 
 intents = discord.Intents.default()
 
@@ -11,23 +14,30 @@ client = commands.Bot(command_prefix='!', intents=intents)
 TOKEN = "" #봇 토큰
 #------
 
-def GetA2SInfo(address:str,port:int):
+async def GetA2SInfo(address:str,port:int):
     try:
-        return(a2s.info((address,port),timeout=1.5))
+        return(await a2s.ainfo((address,port),timeout=1.5))
     except:
         return 0
 
-def GetA2SPlayers(address:str,port:int):
+async def GetA2SPlayers(address:str,port:int):
     try:
-        return(a2s.players((address,port),timeout=1.5))
+        return(await a2s.aplayers((address,port),timeout=1.5))
     except:
         return 0
 
-def GetA2SRules(address:str,port:int):
+async def GetA2SRules(address:str,port:int):
     try:
-        return(a2s.rules((address,port),timeout=1.5))
+        return(await a2s.arules((address,port),timeout=1.5))
     except:
         return 0
+
+def AddPlayerToList(playerInfo): #공백 제외 처리 안 함
+    if (playerInfo.name == ""):
+        return "NeedStrip" #NeedStrip 기준으로 공백 닉네임 제거
+    else:
+        duration:str = str(strftime("%H:%M:%S", gmtime(round(int(playerInfo.duration)))))
+        return f"+ {duration} | {playerInfo.name}"
 
 @client.event
 async def on_ready():
@@ -38,8 +48,22 @@ async def server_scan(ctx,address:str=None,port:int=None):
     if (address == None or port == None):
         await ctx.send("오류: 인자 누락") #discord.ext.commands.errors.MissingRequiredArgument 예외 처리 실패
         return
-    await ctx.send(GetA2SInfo(address,port))
-    await ctx.send(GetA2SPlayers(address,port))
-    await ctx.send(GetA2SRules(address,port))
+
+    rules,info,players = await asyncio.gather(
+        GetA2SRules(address,port),
+        GetA2SInfo(address,port),
+        GetA2SPlayers(address,port)
+    )
+    
+    if (rules == 0 or info == 0 or players == 0):
+        await ctx.send("오류: 서버 스캔 실패")
+        return
+
+    header:str = f'```ahk\n"Server Name" : "{info.server_name}"\n"Map Name" : "{info.map_name}"\n"Player Count" : "{info.player_count}/{info.max_players}"```'
+    playerList:list = list(map(AddPlayerToList,players))
+    body:str = "```diff\n{0}```".format('\n'.join(playerList)).replace("NeedStrip\n","")
+
+    await ctx.send(header)
+    await ctx.send(body)
 
 client.run(TOKEN)
